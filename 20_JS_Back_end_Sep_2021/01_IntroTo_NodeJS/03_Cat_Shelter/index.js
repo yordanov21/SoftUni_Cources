d499cb65ccf9c563c7f4417bfe417c3d0f218ae9
Yordan Yordanov <y.yordanov90@gmail.com>
2021-09-22 16:33:57 +0300

    Node.js Cat Shelter. Home page visualization

--- 20_JS_Back_end_Sep_2021/01_IntroTo_NodeJS/03_Cat_Shelter/index.js
+++ 20_JS_Back_end_Sep_2021/01_IntroTo_NodeJS/03_Cat_Shelter/index.js
@@ -1,12 +1,30 @@
+const fs = require('fs');
 const http = require('http');
 
 
 const app = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
-
+            let content = fs.readFileSync('./views/home/index.html');
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(content);
             break;
-
+        case '/styles/site.css':
+            let css = fs.readFileSync('./styles/site.css');
+            res.writeHead(200, {
+                'Content-Type': 'text/css'
+            });
+            res.write(css);
+            break;
+        case '/js/script.js':
+            let jsScript = fs.readFileSync('./js/script.js');
+            res.writeHead(200, {
+                'Content-Type': 'text/javascript'
+            });
+            res.write(jsScript);
+            break;
         default:
             res.statusCode = 404;
             break;
