03a3ae56550b138ece33074e7e5cdf8de8013829
Yordan Yordanov <y.yordanov90@gmail.com>
2021-09-26 09:30:59 +0300

    setup the express

--- 20_JS_Back_end_Sep_2021/04_ExpressTempleitingWorkshop/cubicle/src/index.js
+++ 20_JS_Back_end_Sep_2021/04_ExpressTempleitingWorkshop/cubicle/src/index.js
@@ -1,1 +1,15 @@
-console.log('lets start')
+const express = require('express');
+
+const app = express();
+
+app.all('/', (req, res) => {
+    res.write('It\s working')
+    res.end();
+});
+
+// with binding 
+app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
+// with arrow function
+// app.listen(5000, () => {
+//     console.log('Application is running on http://localhost:5000');
+// })
