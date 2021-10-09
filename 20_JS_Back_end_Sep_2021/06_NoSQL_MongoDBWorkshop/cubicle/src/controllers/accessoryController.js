1dd8a7f46087016e97ecf260cf5a744058dfa8f9
Yordan Yordanov <y.yordanov90@gmail.com>
2021-10-10 00:17:27 +0300

    Add accessory model and accessory create page

---
+++ 20_JS_Back_end_Sep_2021/06_NoSQL_MongoDBWorkshop/cubicle/src/controllers/accessoryController.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+//const accessoryService = require('../services/accessoryService');
+
+router.get('/create', (req, res) => {
+    res.render('accessory/create'); // accessory is the folder in the views folder and create is the create.hbs file
+});
+
+router.post('/create', async (req, res) => {
+    let { name, description, imageUrl } = req.body;
+
+    await accessoryService.create(name, description, imageUrl)
+
+    res.redirect('/');
+});
+
+module.exports = router;
