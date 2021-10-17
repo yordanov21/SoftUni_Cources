d821a1314ac2f337628257c768c5904cfa77ee3b
Yordan Yordanov <y.yordanov90@gmail.com>
2021-10-17 16:23:56 +0300

    add edit cube functionality

--- 20_JS_Back_end_Sep_2021/08_SessionAndAuthWorkshop/cubicle/src/controllers/cubeController.js
+++ 20_JS_Back_end_Sep_2021/08_SessionAndAuthWorkshop/cubicle/src/controllers/cubeController.js
@@ -33,8 +33,15 @@ const cubeDetails = async (req, res) => {
 const getEditCubePage = async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
     res.render('cube/edit', { ...cube });
-    // res.render('cube/edit');
 }
+
+const postEditCubePage = async (req, res) => {
+    let { name, description, imageUrl, difficulty } = req.body;
+
+    await cubeService.updateOne(req.params.cubeId, { name, description, imageUrl, difficulty })
+
+    res.redirect(`/cube/${req.params.cubeId}`);
+};
 
 const getDeleteCubePage = async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
@@ -52,6 +59,7 @@ router.get('/create', isAuth, getCreateCubePage);
 router.post('/create', isAuth, createCube);
 router.get('/:cubeId', cubeDetails);
 router.get('/:cubeId/edit', isAuth, getEditCubePage);
+router.post('/:cubeId/edit', isAuth, postEditCubePage);
 router.get('/:cubeId/delete', isAuth, getDeleteCubePage);
 router.post('/:cubeId/delete', isAuth, postDeleteCubePage);
 
