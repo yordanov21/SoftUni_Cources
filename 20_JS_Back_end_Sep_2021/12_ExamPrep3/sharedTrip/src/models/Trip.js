a334890d4a29a6a9d6cea105f14872107e176adb
Yordan Yordanov <y.yordanov90@gmail.com>
2021-10-26 21:01:34 +0300

    add trip schema

---
+++ 20_JS_Back_end_Sep_2021/12_ExamPrep3/sharedTrip/src/models/Trip.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+let tripSchema = new mongoose.Schema({
+    startPoint: {
+        type: String,
+        required: true,
+    },
+    endPoint: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    carImage: {
+        type: String,
+        required: true,
+    },
+    carBrand: {
+        type: String,
+        required: true,
+    },
+    seats: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    buddies: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+}, {
+    timestamps: true
+});
+
+
+
+let Trip = mongoose.model('Trip', tripSchema)
+
+module.exports = Trip;
