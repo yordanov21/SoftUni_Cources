84e3453b098b7905935979524295e99f8178e1bb
Yordan Yordanov <y.yordanov90@gmail.com>
2021-11-20 21:02:29 +0200

    Example without JXS syntax

--- 21_ReactJS/01_IntroToReactAndJSX/plainReact/src/script.js
+++ 21_ReactJS/01_IntroToReactAndJSX/plainReact/src/script.js
@@ -1,1 +1,10 @@
 let rootElement = document.getElementById('root');
+
+let reactElement = React.createElement(
+    'header',
+    { className: 'side-header' },
+    React.createElement('h1', { id: 'main-heading' }, 'Hello react!'),
+    React.createElement('h2', {}, 'The best framework it the world'),
+);
+
+ReactDOM.render(reactElement, rootElement);
