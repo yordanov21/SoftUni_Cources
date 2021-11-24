bb4797f14400aa25397d69d561f936729d8d936f
Yordan Yordanov <y.yordanov90@gmail.com>
2021-11-24 22:03:29 +0200

    state example and debug

--- 21_ReactJS/02_Components_BasicIdea/react-components-demo/src/components/ToDoList.js
+++ 21_ReactJS/02_Components_BasicIdea/react-components-demo/src/components/ToDoList.js
@@ -1,22 +1,50 @@
+import React from 'react';
+
 import ToDoListItem from './ToDoListItem';
 
 function ToDoListFuncComponent() {
+    // put always in the begining ot the component
+    let todoState = React.useState(['Clean your room 2', 'Go Shoping 2', 'Learn React 2', 'Wash the dishes 2']);
+    // array destructuring assignment
+    let [todos, setTodos] = todoState;
 
-    let firstTask = "Clean your room";
-    let firstColor = 'blue';
-    let person = {
-        name: "Pehso",
-        age: 20
-    };
+    let [todos2, setTodos2] = React.useState('initial todo');
+    let [count, setCount] = React.useState(1);
+
+    console.log(todos2);
+    console.log(setTodos2);
+
+
+    console.log(todos);
+    console.log(setTodos);
+
+
+    // for func component demo
+    // let firstTask = "Clean your room";
+    // let firstColor = 'blue';
+    // let person = {
+    //     name: "Pehso",
+    //     age: 20
+    // };
 
     return (
         <>
             <h2>Tasks</h2>
             <ul>
-                <ToDoListItem color={firstColor}>{firstTask}</ToDoListItem>
+                {/* // demo for state */}
+                {todos.map(todo => <ToDoListItem>{todo}</ToDoListItem>)}
+
+                <ToDoListItem>{todos2}</ToDoListItem>
+
+                {/* // demo for function components */}
+                {/* <ToDoListItem color={firstColor}>{firstTask}</ToDoListItem>
                 <ToDoListItem color="green"><p>Go Shoping - func</p></ToDoListItem>
                 <ToDoListItem color="red" person={person}>Learn React - func</ToDoListItem>
-                <ToDoListItem color="purple">Wash the dishes - func</ToDoListItem>
+                <ToDoListItem color="purple">Wash the dishes - func</ToDoListItem> */}
+
+                <button onClick={() => console.log('click')}>Modify</button>
+                <button onClick={() => setCount(++count)}>Rise count</button>
+                <p>{count}</p>
             </ul>
         </>
     );
