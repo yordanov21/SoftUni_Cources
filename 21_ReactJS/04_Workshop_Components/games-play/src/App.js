0c0b928c26985e0cac245e0cf3c900cfc8a7a08c
Yordan Yordanov <y.yordanov90@gmail.com>
2021-11-26 23:56:25 +0200

    Add simple page router

--- 21_ReactJS/04_Workshop_Components/games-play/src/App.js
+++ 21_ReactJS/04_Workshop_Components/games-play/src/App.js
@@ -1,3 +1,5 @@
+import { useState, createElement } from 'react';
+
 import Header from './components/Header';
 import WelcomeWorld from './components/WelcomeWorld';
 import Login from './components/Login';
@@ -8,18 +10,25 @@ import GameDetails from './components/GameDetails';
 import GameCatalog from './components/GameCatalog';
 
 function App() {
+	const [page, setPage] = useState('/home');
+
 	const routers = {
-		'/home': WelcomeWorld,
-		'/games': GameCatalog,
-		'/create-game': CreateGame,
+		'/home': <WelcomeWorld />,
+		'/games': <GameCatalog />,
+		'/create-game': <CreateGame />,
+	}
+
+	const navigationChangeHandler = (path) => {
+		console.log(path);
+		setPage(path);
 	}
 
 	return (
 		<div id="box">
-			<Header />
+			<Header navigationChangeHandler={navigationChangeHandler} />
 
 			<main id="main-content">
-				<WelcomeWorld />
+				{routers[page] || <h2>No Page Found!</h2>}
 			</main>
 
 		</div>
--- 21_ReactJS/04_Workshop_Components/games-play/src/components/Header.js
+++ 21_ReactJS/04_Workshop_Components/games-play/src/components/Header.js
@@ -1,12 +1,12 @@
-const Header = () => {
+const Header = ({
+    navigationChangeHandler,
+}) => {
     const onHeaderClick = (e) => {
         e.preventDefault()
 
         if (e.target.tagName == 'A') {
             let url = new URL(e.target.href);
-
-            console.log(url.pathname);
-
+            navigationChangeHandler(url.pathname);
         }
     };
 
