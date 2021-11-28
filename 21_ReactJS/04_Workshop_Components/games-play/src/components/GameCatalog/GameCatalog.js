68c3927ed5b74f85d46e1cc7ecc91f31cc493fc1
Yordan Yordanov <y.yordanov90@gmail.com>
2021-11-28 21:04:15 +0200

    Show no games yet message

--- 21_ReactJS/04_Workshop_Components/games-play/src/components/GameCatalog/GameCatalog.js
+++ 21_ReactJS/04_Workshop_Components/games-play/src/components/GameCatalog/GameCatalog.js
@@ -6,21 +6,25 @@ const GameCatalog = () => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
-            .then(result => {
-                console.log(result);
-                setGames(result)
-            });
+        setTimeout(() => {
+            fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+                .then(res => res.json())
+                .then(result => {
+                    console.log(result);
+                    setGames(result)
+                });
+        }, 1000);
+
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map(x => <GameCard game={x} />)}
-
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0
+                ? games.map(x => <GameCard key={x._id} game={x} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
         </section>
     );
 };
