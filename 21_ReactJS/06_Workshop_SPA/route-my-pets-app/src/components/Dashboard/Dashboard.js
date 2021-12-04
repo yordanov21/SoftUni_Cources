ce16b8e925a3f75e1879c68f6f70fad9ec67d6af
Yordan Yordanov <y.yordanov90@gmail.com>
2021-12-04 21:58:34 +0200

    Refactoring petList and petCard

--- 21_ReactJS/06_Workshop_SPA/route-my-pets-app/src/components/Dashboard/Dashboard.js
+++ 21_ReactJS/06_Workshop_SPA/route-my-pets-app/src/components/Dashboard/Dashboard.js
@@ -1,30 +1,19 @@
-
-import { useEffect, useState } from "react";
-import PetCard from "../PetCard/PetCard";
-import * as petService from '../../services/petService';
-
+import { Routes, Route } from "react-router-dom";
+import PetList from '../PetList/PetList';
 
 const Dashboard = () => {
-
-    const [pets, setPets] = useState([]);
-
-    useEffect(() => {
-        petService.getAll()
-            .then(result => {
-                console.log(result);
-                setPets(result);
-            })
-    }, []);
 
     return (
         <section id="dashboard-page" className="dashboard">
             <h1>Dashboard</h1>
 
-            <ul className="other-pets-list">
-                {pets.map(x => <PetCard key={x._id} pet={x} />)}
-            </ul>
+            <nav>
+                <Routes>
+                    <Route path="/" element={<PetList />} />
+                </Routes>
 
-            <p className="no-pets">No pets in database!</p>
+            </nav>
+
         </section>
     );
 };
